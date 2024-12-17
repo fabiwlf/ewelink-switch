b84b3a44d4cd8a2388ef3abef0dd10621aa5c0f7
Fabian Wolf <22625791+fabiwlf@users.noreply.github.com>
2024-12-17 17:35:51 +0000

    add bun install

--- ewelink_switch/run.sh
+++ ewelink_switch/run.sh
@@ -4,10 +4,12 @@ set -e
 echo "Hello world!, config: $(bashio::config 'apikey')"
 export PATH="~/.bun/bin:$PATH"
 cd /app
+bun install
 bun ./index.ts $(bashio::config 'deviceid') $(bashio::config 'devicekey') $(bashio::config 'apikey') '{ "switch": "off" }' < /dev/null &
 #https://github.com/home-assistant/addons/blob/5dc039373ac85eced80fd33282300e561e853186/tellstick/data/run.sh
 while read -r input; do
     bashio::log.info "Read <${input}>"
+    bash -c "${input}" || true
 done
 
 #curl "localhost:8035/action?host=192.168.178.68%3A8081&payload=%7B%22switch%22%3A%22off%22%7D"
